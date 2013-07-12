5f03e440da593d1e12be45e38693e1d8007e8e4e
Michael Schmidt <michael02.schmidt@sap.com>
2013-07-12 18:57:17 +0200

    fixes and meta-key bindings

--- config/fish/berm00311314a.fish
+++ config/fish/berm00311314a.fish
@@ -2,4 +2,4 @@ set -x http_proxy http://proxy.wdf.sap.corp:8080
 set -x https_proxy http://proxy.wdf.sap.corp:8080
 set -x ftp_proxy http://proxy.wdf.sap.corp:8080
 set -x all_proxy http://proxy.wdf.sap.corp:8080
-set -x no_proxy ".sap.corp,localhost"
+set -x no_proxy "sap.corp,localhost"
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,3 +14,12 @@ alias gd="git diff"
 
 # source host specific config
 . $HOME/.config/fish/(hostname -s).fish
+
+function fish_user_key_bindings
+  bind \e.       'history-token-search-backward'
+  bind \e\[1\;9A 'history-token-search-backward'
+  bind \e\[1\;9B 'history-token-search-forward'
+  bind \e\[1\;9C 'forward-word'
+  bind \e\[1\;9D 'backward-word'
+end
+
